a73f643f8fae0485891510d5e34ec94d7140e8f0
Winson <Winson@Winson>
2014-05-28 15:47:47 +0800

    addPost collectPost cancelPost likePost cancelLike

--- GuitarWebApp/WebContent/pages/js/EventHandle.js
+++ GuitarWebApp/WebContent/pages/js/EventHandle.js
@@ -1,24 +1,53 @@
 // JavaScript Document
 //create event source
+$(document).ready(function(){
 window.onload = function(){
 if (!!window.EventSource) {
 		var source = new EventSource("../../GuitarWebApp/app/event/subscribe");
 		source.addEventListener("ADDPOST",function(event){
 		var jsondata = $.parseJSON(event.data);
 		var jsonPostShortCut = jsondata.postShortCut;
-		var boarddiv = "<div class='post'><div class='post_body'><div class='row'><div class='col-md-2'><div class='user_img'><img src='images/user_img.jpg' /></div></div><div class='col-md-6'><div class='user_name'><strong>"+jsonPostShortCut.ownerNickName+"</strong></div><div class='user_info'>"+jsonPostShortCut.publishDate+"</div></div></div><div class='post_info'>"+jsonPostShortCut.content+"<div class='post_more'><a>read more...</a></div></div><div class='post_img'><img src='images/13.png' /></div><div class='row'><div class='col-md-1'><div class='like'><a><span class='glyphicon glyphicon-heart-empty' style='font-size:20px'>"+jsonPostShortCut.likeNum+"</span></a></div></div><div class='col-md-1'><div class='post_share'><a><span class='glyphicon glyphicon-share-alt' style='font-size:20px'></span></a></div></div></div></div></div>";
+		 var boarddiv = "<div class='post'><div class='post_body'><div class='row'><div class='col-md-2'><div class='user_img'><img src='images/user_img.jpg' /></div></div><div class='col-md-6'><div class='user_name'><strong>"+jsonPostShortCut.ownerNickName+"</strong></div><div class='user_info'>"+jsonPostShortCut.publishDate+"</div></div> </div><div class='post_info'>"+jsonPostShortCut.content+"<div class='post_more'><a>read more...</a></div></div><div class='post_img'><img src='images/9.jpg' /></div><div class='row'><div class='col-md-1'><div class='post_like' style='cursor:pointer'><a><input id='likeID' type='hidden' value="+jsonPostShortCut.ID+"><span class='glyphicon glyphicon-heart-empty' style='font-size:20px'>"+jsonPostShortCut.likeNum+"</span></a></div></div><div class='col-md-1'><div class='post_collect' style='cursor:pointer'><a><input id='collectID' type='hidden' value="+jsonPostShortCut.ID+"><span class='glyphicon glyphicon-star-empty' style='font-size:20px'></span></a></div></div><div class='col-md-1'><div class='post_share' style='cursor:pointer'><a><span class='glyphicon glyphicon-share-alt' style='font-size:20px'></span></a></div></div></div></div></div>";
 		//append div
 		$(".share").after(boarddiv); 
-		Msnry();
+		Msnry('.pro_body','.post',435);
  });
 		source.addEventListener('deletePostSSE',function(e){});
-		source.addEventListener('likePostSSE',function(e){});
-		source.addEventListener('collectPostSSE',function(e){});
-		source.addEventListener('cancelPostSSE',function(e){});
-		source.addEventListener('joinActivitySSE',function(e){});
+		source.addEventListener('LIKEPOST',function(event){
+			var jsondata = $.parseJSON(event.data);
+			var postID = jsondata.pcID;
+			var inputID = $("input[value='"+postID+"'][id='likeID']");
+			inputID.next().attr("class","glyphicon glyphicon-heart");
+			var like = parseInt(inputID.next().text())+1;
+			inputID.next().text(like); 
+		});
+		source.addEventListener('CANCELLIKE',function(event){
+			var jsondata = $.parseJSON(event.data);
+			var postID = jsondata.pcID;
+			var inputID = $("input[value='"+postID+"'][id='likeID']");
+			inputID.next().attr("class","glyphicon glyphicon-heart-empty");
+			var like = parseInt(inputID.next().text())-1;
+			inputID.next().text(like);
+		});
+		source.addEventListener('COLLECTPOST',function(event){
+			var jsondata = $.parseJSON(event.data);
+			var postID = jsondata.pcID;
+			var inputID = $("input[value='"+postID+"'][id='collectID']");
+			inputID.next().attr("class","glyphicon glyphicon-star");
+		});
+		source.addEventListener('CANCELCOLLECT',function(event){
+			var jsondata = $.parseJSON(event.data);
+			var postID = jsondata.pcID;
+			var inputID = $("input[value='"+postID+"'][id='collectID']");
+			inputID.next().attr("class","glyphicon glyphicon-star-empty");
+		});
+		source.addEventListener('JOINACTIVITY',function(e){
+			alert("success!");
+		});
 		source.addEventListener('addCommentSSE',function(e){});
 		source.addEventListener('supportSSE',function(e){});
 		source.addEventListener('cancelSupportSSE',function(e){});
 		source.addEventListener('followSSE',function(e){});
 }
 };
+});
--- GuitarWebApp/WebContent/pages/js/function.js
+++ GuitarWebApp/WebContent/pages/js/function.js
@@ -1,13 +1,94 @@
 
-	function Msnry(){
-		var container = document.querySelector('.pro_body');
+	function Msnry(selectContain,item,width){
+		var container = document.querySelector(selectContain);
 		var msnry;
 		imagesLoaded( container, function() {
 		msnry = new Masonry( container,{
-			columnWidth: 435,
-			itemSelector: '.post',
+			columnWidth: width,
+			itemSelector: item,
 			gutter:20
 			});
 		});
 	}
-	Msnry();
+$(document).ready(function(){
+//function addPost
+		$("#btn_share").click(function() {
+		//get urls
+		var formData = new FormData($('form')[0]);
+		$.ajax({
+			type:'POST',
+			url:'../../GuitarWebApp/app/fileUploader',
+	        //Ajax events
+	        success:function(data){
+	        	var d = new Date();
+	        	var publishDateN = d.getFullYear() + "/" +(d.getMonth()+1) + "/" + d.getDate();
+	        	var date = new Date(publishDateN);
+	        	var jtopic = $("#topic").val();
+	    		var jcontent = $("#share_txt").val();
+	    		var jtype = $("#type").val();
+	    		var jpublishDate = $("#publishDate").val();
+	    		var jstartDate = $("#startDate").val();
+	    		var jsonString = {mediaLocation:data,topic:jtopic,content:jcontent,postType:"ACTIVITY",publishDate:date,startDate:date};
+	    		var jsonData = $.toJSON(jsonString);
+	    		$.ajax({
+	    			type:"POST",
+	    			url:'../../GuitarWebApp/app/post/add/2011052407',
+	    			data:jsonData,
+	    			contentType: "application/json"
+	    		});
+				},
+	        // Form data
+	        data: formData,
+	        //Options to tell jQuery not to process data or worry about content-type.
+	        cache: false,
+	        contentType: false,
+	        processData: false
+		});
+					
+	});		
+
+//function collectPost and cancelCollcet
+		$('body').on('click','.post_collect',function() {
+			if(this.lastChild.lastChild.getAttribute("class") == "glyphicon glyphicon-star-empty"){
+				var id = this.firstChild.firstChild.getAttribute("value");
+				$.ajax({
+					url:'../../GuitarWebApp/app/post/collect/2011052407/'+id,//'../../GuitarWebApp/app/post/collect/2011052407/'+id
+					type:'put'
+				});
+			}
+			if(this.lastChild.lastChild.getAttribute("class") == "glyphicon glyphicon-star"){
+				var id = this.firstChild.firstChild.getAttribute("value");
+				$.ajax({
+					url:'../../GuitarWebApp/app/post/cancelCollect/2011052407/'+id,//'../../GuitarWebApp/app/post/collect/2011052407/'+id
+					type:'put'
+				});
+			}
+		});
+//function likePost and cancelLike
+		$('body').on('click','.post_like',function(){
+			if(this.lastChild.lastChild.getAttribute("class") == "glyphicon glyphicon-heart-empty"){
+				var id = this.firstChild.firstChild.getAttribute("value");
+				$.ajax({
+					url:'../../GuitarWebApp/app/post/like/2011052407/'+id,//'../../GuitarWebApp/app/post/collect/2011052407/'+id
+					type:'put'
+				});
+			}
+			if(this.lastChild.lastChild.getAttribute("class") == "glyphicon glyphicon-heart"){
+				var id = this.firstChild.firstChild.getAttribute("value");
+				$.ajax({
+					url:'../../GuitarWebApp/app/post/cancelLike/2011052407/'+id,//'../../GuitarWebApp/app/post/collect/2011052407/'+id
+					type:'put'
+				});
+			}
+		});
+//function likePost and cancelLike
+		$('body').on('click','#activityJoin',function(){
+			var id = this.firstChild.firstChild.getAttribute("value");
+			$.ajax({
+				url:'../../GuitarWebApp/app/post/join/2011052407/1',//'../../GuitarWebApp/app/post/collect/2011052407/'+id
+				type:'put'
+			});
+		});
+		
+});
+
