bbde2c1556c7451dfbcaf8a3e89e407a5d324579
Winson <Winson@Winson>
2014-05-29 14:41:43 +0800

    addcomment problem

--- GuitarWebApp/WebContent/pages/js/EventHandle.js
+++ GuitarWebApp/WebContent/pages/js/EventHandle.js
@@ -7,7 +7,7 @@ if (!!window.EventSource) {
 		source.addEventListener("ADDPOST",function(event){
 		var jsondata = $.parseJSON(event.data);
 		var jsonPostShortCut = jsondata.postShortCut;
-		 var boarddiv = "<div class='post'><div class='post_body'><div class='row'><div class='col-md-2'><div class='user_img'><img src='images/user_img.jpg' /></div></div><div class='col-md-6'><div class='user_name'><strong>"+jsonPostShortCut.ownerNickName+"</strong></div><div class='user_info'>"+jsonPostShortCut.publishDate+"</div></div> </div><div class='post_info'>"+jsonPostShortCut.content+"<div class='post_more'><a>read more...</a></div></div><div class='post_img'><img src='images/9.jpg' /></div><div class='row'><div class='col-md-1'><div class='post_like' style='cursor:pointer'><a><input id='likeID' type='hidden' value="+jsonPostShortCut.ID+"><span class='glyphicon glyphicon-heart-empty' style='font-size:20px'>"+jsonPostShortCut.likeNum+"</span></a></div></div><div class='col-md-1'><div class='post_collect' style='cursor:pointer'><a><input id='collectID' type='hidden' value="+jsonPostShortCut.ID+"><span class='glyphicon glyphicon-star-empty' style='font-size:20px'></span></a></div></div><div class='col-md-1'><div class='post_share' style='cursor:pointer'><a><span class='glyphicon glyphicon-share-alt' style='font-size:20px'></span></a></div></div></div></div></div>";
+		 var boarddiv = "<div class='post'><div class='post_body'><div class='row'><div class='col-md-2'><div class='user_img'><img src='images/user_img.jpg' /></div></div><div class='col-md-6'><div class='user_name'><strong>"+jsonPostShortCut.ownerNickName+"</strong></div><div class='user_info'>"+jsonPostShortCut.publishDate+"</div></div> </div><div class='post_info'>"+jsonPostShortCut.content+"<div class='post_more'><a>read more...</a></div></div><div class='post_img'><img src='images/9.jpg' /></div><div class='row'><div class='col-md-1'><div class='post_like' style='cursor:pointer'><a><input id='likeID' type='hidden' value="+jsonPostShortCut.ID+"><span class='glyphicon glyphicon-heart-empty' style='font-size:20px'>"+jsonPostShortCut.likeNum+"</span></a></div></div><div class='col-md-1'><div class='post_collect' style='cursor:pointer'><a><input id='collectID' type='hidden' value="+jsonPostShortCut.ID+"><span class='glyphicon glyphicon-star-empty' style='font-size:20px'></span></a></div></div><div class='col-md-1'><div class='post_share' style='cursor:pointer'><a><span class='glyphicon glyphicon-share-alt' style='font-size:20px'></span></a></div></div></div><div class='media_comm'><div class='row addCommentBtn'><div class='col-lg-8'><div class='form-group'><input type='text' placeholder='Add a comment' class='form-control'></div></div><div class='col-lg-4'><button type='submit' class='btn btn-success' id='addComment'><input id='commentID' type='hidden' value="+jsonPostShortCut.ID+">Summit</button></div></div><div class='act_content'><div class='row'><div class='col-lg-1'><img src='images/user_img2.jpg' /></div><div class='col-lg-11'><div class='ures_name'><strong>Winson_Lau</strong></div><div class='user_info'>Yesterday 21:23pm</div></div></div><div class='act_comment'>There's an extension that whenever you choose something that has a shortcut it has a toast popup that tells you what the shortcut would have been. I used it briefly a long time ago. But it turned out to be really annoying. Still it was a great idea.﻿</div></div><div class='act_content'><div class='row'><div class='col-lg-1'><img src='images/user_img3.jpg' /></div><div class='col-lg-11'><div class='ures_name'><strong>Thackoor Singh</strong></div><div class='user_info'>Yesterday 21:23pm</div></div></div><div class='act_comment'>free!</div></div></div></div></div>";
 		//append div
 		$(".share").after(boarddiv); 
 		Msnry('.pro_body','.post',435);
@@ -44,10 +44,20 @@ if (!!window.EventSource) {
 		source.addEventListener('JOINACTIVITY',function(e){
 			alert("success!");
 		});
-		source.addEventListener('addCommentSSE',function(e){});
+		source.addEventListener('ADDCOMMENT',function(event){
+			var jsondata = $.parseJSON(event.data);
+			var jsonComment = jsondata.comment;
+			var boarddiv = "<div class='act_content'><div class='row'><div class='col-lg-1'><img src='images/user_img2.jpg' /></div><div class='col-lg-11'><div class='ures_name'><strong>"+jsondata.userID+"</strong></div><div class='user_info'>Yesterday 21:23pm</div></div></div><div class='act_comment'>"+jsonComment.content+"﻿</div></div></div>";
+			$(".addCommentBtn").after(boarddiv); 
+		});
 		source.addEventListener('supportSSE',function(e){});
 		source.addEventListener('cancelSupportSSE',function(e){});
-		source.addEventListener('followSSE',function(e){});
+		source.addEventListener('FOLLOW',function(event){
+			var jsondata = $.parseJSON(event.data);
+			var followBtn = $("button[id='followBtn']");
+			followBtn.text("Following");
+			
+		});
 }
 };
 });
--- GuitarWebApp/WebContent/pages/js/function.js
+++ GuitarWebApp/WebContent/pages/js/function.js
@@ -1,4 +1,4 @@
-
+//function Msnry
 	function Msnry(selectContain,item,width){
 		var container = document.querySelector(selectContain);
 		var msnry;
@@ -10,7 +10,55 @@
 			});
 		});
 	}
+	
 $(document).ready(function(){
+//function userTip
+	(function($){  
+		$.fn.userTips = function () {
+
+			var tipFrame = '<div class="popTip"><div class="content"><div class="urserBgShort"><img src="images/urseBgShort.jpg" /></div><div class="urserInfShort"><img src="images/user_img4.jpg" /><p><h1>Bond</h1></p><p> a good guy</p><button>follow</button></div></div></div>';
+			
+			// Speed of the animations in milliseconds - 1000 = 1 second.
+			var animSpeed = 300;
+							
+			// Global tinyTip variables;
+			var tinyTip;				
+			// When we hover over the element that we want the tooltip applied to
+			$(this).hover(function() {		
+				$('body').append(tipFrame);
+				var divTip = 'div.popTip';
+				tinyTip = $(divTip);
+				tinyTip.hide();
+				
+				// Offsets so that the tooltip is centered over the element it is being applied to but
+				// raise it up above the element so it isn't covering it.
+				var yOffset = tinyTip.height();
+				var xOffset = (tinyTip.width() / 2) - ($(this).width() / 2);
+				
+				// Grab the coordinates for the element with the tooltip and make a new copy
+				// so that we can keep the original un-touched.
+				var pos = $(this).offset();
+				var nPos = pos;
+				
+				// Add the offsets to the tooltip position
+				nPos.top = pos.top - yOffset;
+				nPos.left = pos.left - xOffset;
+				
+				// Make sure that the tooltip has absolute positioning and a high z-index, 
+				// then place it at the correct spot and fade it in.
+				tinyTip.css('position', 'absolute').css('z-index', '1000');
+				tinyTip.css(nPos).fadeIn(animSpeed);
+				
+			}, function() {
+				// Fade the tooltip out once the mouse moves away and then remove it from the DOM.
+				tinyTip.fadeOut(animSpeed, function() {
+					$(this).remove();
+					//$('div.popTip').remove();
+				});
+			});				
+		};
+	})(jQuery);
+	$('img.userImg').userTips();
 //function addPost
 		$("#btn_share").click(function() {
 		//get urls
@@ -90,6 +138,34 @@ $(document).ready(function(){
 			});
 		});
 		
+//function addComment
+		$('body').on('click','#addComment',function(){
+			var d = new Date();
+        	var publishDateN = d.getFullYear() + "/" +(d.getMonth()+1) + "/" + d.getDate();
+        	var date = new Date(publishDateN);
+			var jcontent = $(".form-control").val();
+			var jCommentType = "COMMENT";
+			var jpublishDate = $("#publishDate").val();
+			var jsonString = {content:jcontent,commentType:jCommentType};
+			var jsonData = $.toJSON(jsonString);
+			var id = this.firstChild.getAttribute("value");
+			$.ajax({
+    			type:"POST",
+    			url:'../../GuitarWebApp/app/comment/add/2011052407/'+id,//'../../GuitarWebApp/app/comment/add/2011052407/'+id
+    			data:jsonData,
+    			contentType: "application/json"
+    		});
+		});
+//function follow
+		$('body').on('click','#followBtn',function(){
+			//get post owner
+			if($(this).text()=="follow"){
+				$.ajax({
+					url:'../../GuitarWebApp/app/user/follow/2011052407/2011052406',//'../../GuitarWebApp/app/user/follow/2011052407/'+id
+					type:'put'
+				});
+			}
+		});
 		
 });
 
